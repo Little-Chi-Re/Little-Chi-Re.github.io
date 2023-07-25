f83a10e1fa76cb5c34d8f1ffa29649d084b80127
Administrator <3531962676@qq.com>
2023-07-25 22:31:02 +0800

    chire

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# 这是一个示例 Python 脚本。
+from flask import Flask
 
-# 按 Shift+F10 执行或将其替换为您的代码。
-# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
-
-
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
+app = Flask(__name__)
 
 
-# 按间距中的绿色按钮以运行脚本。
+@app.route('/', methods=['GET', 'POST'])
+def root():
+    return '?'
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
+    # print(scanDir(comicsList()[0]))
+    app.run(host="0.0.0.0", debug=True)
